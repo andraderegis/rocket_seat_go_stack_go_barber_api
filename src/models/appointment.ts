e29a6520c173dbf04728396c1e9168644fbf92d7
andraderegis <reginaldo.andrade@outlook.com>
2021-01-23 16:01:57 -0300

    Remove unused dependency from appointment model

--- src/models/appointment.ts
+++ src/models/appointment.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn, Generated } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('appointments')
 class Appointement {
