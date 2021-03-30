9bd891e3cc2001f499eac60e501c53e6c8f2e69d
andraderegis <reginaldo.andrade@outlook.com>
2021-03-30 20:15:42 -0300

    Finish tests for AuthenticateUserService

--- src/modules/users/services/tests/AuthenticateUserService.spec.ts
+++ src/modules/users/services/tests/AuthenticateUserService.spec.ts
@@ -6,6 +6,8 @@ import CreateUserService from '@modules/users/services/CreateUserService';
 import MockUsersRepository from '@modules/users/repositories/mocks/MockUsersRespository';
 import MockHashProvider from '@modules/users/providers/hash/mocks/MockHashProvider';
 
+import AppError from '@shared/errors/AppError';
+
 describe('AuthenticateUser', () => {
   it('should be able authenticate user', async () => {
     const mockHashProvider = new MockHashProvider();
@@ -34,5 +36,48 @@ describe('AuthenticateUser', () => {
 
     expect(authenticateResponse).toHaveProperty('token');
     expect(authenticateResponse.user).toEqual(user);
+  });
+  it('should not be able authenticate with non existing user', async () => {
+    const mockHashProvider = new MockHashProvider();
+    const mockUsersRepository = new MockUsersRepository();
+
+    const authenticateUserService = new AuthenticateUserService(
+      mockUsersRepository,
+      mockHashProvider
+    );
+
+    const userToAuthenticate = {
+      name: 'Aerith Gainsborough',
+      email: 'theflowergirl@example.com',
+      password: '123456'
+    };
+
+    expect(authenticateUserService.execute(userToAuthenticate)).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able authenticate user with wrong password', async () => {
+    const mockHashProvider = new MockHashProvider();
+    const mockUsersRepository = new MockUsersRepository();
+
+    const createUserService = new CreateUserService(mockUsersRepository, mockHashProvider);
+    const authenticateUserService = new AuthenticateUserService(
+      mockUsersRepository,
+      mockHashProvider
+    );
+
+    const userToCreate = {
+      name: 'Aerith Gainsborough',
+      email: 'theflowergirl@example.com',
+      password: '123456'
+    };
+
+    await createUserService.execute(userToCreate);
+
+    const userToAuthenticate = {
+      email: userToCreate.email,
+      password: '12345'
+    };
+
+    expect(authenticateUserService.execute(userToAuthenticate)).rejects.toBeInstanceOf(AppError);
   });
 });
