a768ba3434b2075ed4bde85351a6e5ea616da576
andraderegis <reginaldo.andrade@outlook.com>
2021-04-01 10:35:40 -0300

    Add controllers and routes for ResetPasswordService and SendForgotPasswordEmailService.

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointmentsRoutes';
+import passwordRouter from '@modules/users/infra/http/routes/passwordRoutes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessionsRoutes';
 import usersRouter from '@modules/users/infra/http/routes/usersRoutes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
+routes.use('/password', passwordRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/users', usersRouter);
 
