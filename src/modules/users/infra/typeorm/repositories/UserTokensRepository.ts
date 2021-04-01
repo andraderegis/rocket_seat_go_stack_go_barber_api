3ce1bd408709115b58b1d56918625fd234bef278
andraderegis <reginaldo.andrade@outlook.com>
2021-04-01 10:33:39 -0300

    Implementing UserTokenRepository and add migration for UserTokens table

--- src/modules/users/infra/typeorm/repositories/UserTokensRepository.ts
+++ src/modules/users/infra/typeorm/repositories/UserTokensRepository.ts
@@ -1,13 +1,30 @@
+import { getRepository, Repository } from 'typeorm';
+
+import UserToken from '@modules/users/infra/typeorm/entities/UserToken';
+
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
-import UserToken from '../entities/UserToken';
 
 class UserTokensRepository implements IUserTokensRepository {
+  private ormRepository: Repository<UserToken>;
+
+  constructor() {
+    this.ormRepository = getRepository(UserToken);
+  }
+
   public async findByToken(token: string): Promise<UserToken> {
-    throw new Error('Method not implemented.');
+    return this.ormRepository.findOne({
+      where: {
+        token
+      }
+    });
   }
 
   public async generate(user_id: string): Promise<UserToken> {
-    throw new Error('Method not implemented.');
+    const userToken = this.ormRepository.create({
+      user_id
+    });
+
+    return this.ormRepository.save(userToken);
   }
 }
 
