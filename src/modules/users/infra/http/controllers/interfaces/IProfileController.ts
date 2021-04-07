74124b342cb41c5f45d83caf176f2153bd45b9b3
andraderegis <reginaldo.andrade@outlook.com>
2021-04-07 20:27:41 -0300

    Remove interface IProfileController

--- src/modules/users/infra/http/controllers/interfaces/IProfileController.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from 'express';
-
-export default interface IProfileController {
-  get(request: Request, response: Response): Promise<Response>;
-  update(request: Request, response: Response): Promise<Response>;
-}
