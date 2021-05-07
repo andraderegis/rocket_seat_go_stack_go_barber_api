a7bf93c6dfc51628d830f2519e8e95a627dce33f
Reginaldo Andrade <reginaldo.andrade@outlook.com>
2021-05-07 11:32:33 -0300

    Develop to Master (#15)
    * Add support to docker
    
    * Optimize Docker multistage building
    
    * Suport to docker-compose
    
    * fix .env.example
    
    * remove .env.workstation
    
    * Finish go-barber docker-compose. Add nginx docker-compose
    
    * Fix ormconfig.js
    
    * Cahnge export ports to services mongo, redis and postgresql
    
    * Update github workflow
    
    * Update Run scripts step
    
    * Remove dist step. Change Copy to Digital Ocean step
    
    * Upldate docker and github workflow
    
    * Update docker and github workflow
    
    * Update deploy.sh
    
    * Update docker and docker-compose
    
    * Add redis host env
    
    * Add dev gitaction workflow
    
    * Fix dev gitaction workflow
    
    * Change name dev gitaction workflow
    
    * Change name and branch on dev gitaction workflow
    
    * Fix uses actions checkout  on dev gitaction workflow
    
    * Rename github workflow main.yml to prod.workflow.yml
    
    * Rename title of prod.workflow.yml
    
    * Add step create .env.test file
    
    * Added dockerize support to guarantee gobarber's services dependencies connections

--- docker/go-barber-entrypoint.sh
+++ docker/go-barber-entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+dockerize -wait tcp://postgres:5432 -wait tcp://mongo:27017 -wait tcp://redis:6379 -timeout 20s
+
 ./node_modules/.bin/typeorm migration:run
+
 pm2-runtime start /go_barber_api/dist/shared/infra/http/server.js --name go_barber_api
